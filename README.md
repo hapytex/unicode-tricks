1dd136d78e922d1a14e152ecc97bb8dbdb4f0969
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-04-04 21:24:20 +0200

    added unicode block characters

--- README.md
+++ README.md
@@ -5,3 +5,28 @@
 [![Hackage version badge](https://img.shields.io/hackage/v/unicode-tricks.svg)](https://hackage.haskell.org/package/unicode-tricks)
 
 Based on the [**`unicode`** package](https://hackage.haskell.org/package/unicode) by *Henning Thielemann*.
+
+This library aims to provide functionality to make working with certain blocks of
+unicode characters more effective.
+
+Currently there are three modules:
+
+ - **`Data.Char.Block`**: rendering a 2-by-2 block by a matrix of `Bool`s;
+ - **`Data.Char.Frame`**: typesetting frame elements. Lines of the frames can be
+   `Light` or `Heavy`, and there are additional options to use arcs for corners;
+ - **`Data.Char.Small`**: making use of subscript and superscript in unicode, and
+   for example formatting `Ratio` objects;
+
+## `unicode-tricks` is *safe* Haskell
+
+The modules are marked with the `{-# LANGUAGE Safe #-}` pragma, it thus provides
+guarantees, for example about not using [`unsafePerformIO`](https://begriffs.com/posts/2015-05-24-safe-haskell.html).
+
+## Contribute
+
+You can contribute by making a pull request on the [*GitHub
+repository*](https://github.com/hapytex/unicode-tricks).
+
+You can contact the package maintainer by sending a mail to
+[`hapytexeu+gh@gmail.com`](mailto:hapytexeu+gh@gmail.com).
+
--- src/Data/Char/Block.hs
+++ src/Data/Char/Block.hs
@@ -1,3 +1,71 @@
-{-# LANGUAGE Safe #-}
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, PatternSynonyms, Safe #-}
 
-module Data.Char.Block where
+{-|
+Module      : Data.Char.Block
+Description : A module used to render blocks in unicode.
+Maintainer  : hapytexeu+gh@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+Unicode has 2-by-2 blocks, this module aims to make it more convenient to render such blocks.
+-}
+
+module Data.Char.Block(
+    -- * Datastructures to store the state of the frame.
+    Row(Row, left, right)
+  , Block(Block, upper, lower)
+    -- * A unicode character that is (partially) filled block.
+  , filled
+  ) where
+
+import Test.QuickCheck.Arbitrary(Arbitrary(arbitrary))
+
+
+-- | A data type that determines the state of the /row/ in a block.
+-- it determines the left and the right part of the row of the block.
+data Row a = Row { 
+    left :: a -- ^ The left part of a row of the block.
+  , right :: a -- ^ The right part of the row of the block.
+  } deriving (Eq, Foldable, Functor, Ord, Read, Show, Traversable)
+
+-- | A data type that determines the state of the four subparts of the block.
+data Block a = Block {
+    upper :: Row a -- ^ The upper part of the block.
+  , lower :: Row a -- ^ The lower part of the block.
+  } deriving (Eq, Foldable, Functor, Ord, Read, Show, Traversable)
+
+instance Applicative Row where
+    pure x = Row x x
+    Row fl fr <*> Row l r = Row (fl l) (fr r)
+
+instance Applicative Block where
+    pure x = Block (pure x) (pure x)
+    Block fu fl <*> Block u l = Block (fu <*> u) (fl <*> l)
+
+instance Arbitrary a => Arbitrary (Row a) where
+    arbitrary = Row <$> arbitrary <*> arbitrary
+
+instance Arbitrary a => Arbitrary (Block a) where
+    arbitrary = Block <$> arbitrary <*> arbitrary
+
+-- | Convert the given 'Block' value to a block character in unicode.
+-- 'True' means that part is filled, and 'False' means the part is not filled.
+filled
+    :: Block Bool
+    -> Char
+filled (Block (Row False False) (Row False False)) = ' '
+filled (Block (Row True  True ) (Row False False)) = '\x2580'
+filled (Block (Row False False) (Row True  True )) = '\x2584'
+filled (Block (Row True  True ) (Row True  True )) = '\x2588'
+filled (Block (Row True  False) (Row True  False)) = '\x258c'
+filled (Block (Row False True ) (Row False True )) = '\x2590'
+filled (Block (Row False False) (Row True  False)) = '\x2596'
+filled (Block (Row False False) (Row False True )) = '\x2597'
+filled (Block (Row True  False) (Row False False)) = '\x2598'
+filled (Block (Row True  False) (Row True  True )) = '\x2599'
+filled (Block (Row True  False) (Row False True )) = '\x259a'
+filled (Block (Row True  True ) (Row True  False)) = '\x259b'
+filled (Block (Row True  True ) (Row False True )) = '\x259c'
+filled (Block (Row False True ) (Row False False)) = '\x259d'
+filled (Block (Row False True ) (Row True  False)) = '\x259e'
+filled (Block (Row False True ) (Row True  True )) = '\x259f'
