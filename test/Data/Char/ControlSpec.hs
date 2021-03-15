0e24ff9a42e754f8a978b61e025ec2585214163c
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-03-15 17:38:21 +0100

    added tests for the bidirectional mapping

--- test/Data/Char/ControlSpec.hs
+++ test/Data/Char/ControlSpec.hs
@@ -3,10 +3,11 @@ module Data.Char.ControlSpec (
   ) where
 
 import Data.Char.Control
+import Data.Maybe(isJust)
 
 import Test.Hspec
 import Test.QuickCheck
 
 spec :: Spec
 spec = describe "Data.Char.Control" $
-  it "Check bidirectional mapping" forAll (suchThat (arbitrary :: Gen Char) (isJust . controlPicture)) (\x -> x == fromControlPicture' (controlPicture' x))
+  it "Check bidirectional mapping" (forAll (suchThat (arbitrary :: Gen Char) (isJust . controlPicture)) (\x -> x == fromControlPicture' (controlPicture' x)))
