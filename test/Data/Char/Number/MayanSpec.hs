248fe021d6e0baae9d4d41b98c50f6bd43bed44b
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-05-18 08:43:49 +0200

    include the test objects

---
+++ test/Data/Char/Number/MayanSpec.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Data.Char.Number.MayanSpec
+  ( spec
+  ) where
+
+import Data.Char.Number.Mayan(MayanLiteral)
+import Data.Char.CoreTest
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    testUnicodeCharacter @ MayanLiteral
+    testUnicodeText @ MayanLiteral
