98c0d518eb32041823dc4fbd5f8c57617816a4be
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-07-19 12:28:45 +0200

    add implementation for a sextant

--- src/Data/Char/Block.hs
+++ src/Data/Char/Block.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP, DeriveDataTypeable, DeriveGeneric, DeriveTraversable, FlexibleInstances, Safe #-}
+{-# LANGUAGE CPP, DeriveDataTypeable, DeriveGeneric, DeriveTraversable, FlexibleInstances, PatternSynonyms, Safe #-}
 
 {-|
 Module      : Data.Char.Block
@@ -12,16 +12,21 @@ Unicode has 2-by-2 blocks, this module aims to make it more convenient to render
 
 module Data.Char.Block(
     -- * Datastructures to store the state of the frame.
-    Row(Row, left, right)
+    Row(Row, left, right), rowValue, toRow, toRow'
+  , pattern EmptyRow, pattern FullRow, pattern LeftRow, pattern RightRow
   , Block(Block, upper, lower)
     -- * A unicode character that is (partially) filled block.
   , filled
     -- * Convert a 'Char'acter to a (partially) filled block.
   , fromBlock, fromBlock'
+    -- * Pattern synonyms for blocks
+  , pattern EmptyBlock, pattern FullBlock, pattern LeftHalfBlock, pattern RightHalfBlock
   ) where
 
 import Control.DeepSeq(NFData, NFData1)
 
+import Data.Bits((.|.))
+import Data.Bool(bool)
 import Data.Char.Core(MirrorHorizontal(mirrorHorizontal), MirrorVertical(mirrorVertical), UnicodeCharacter(toUnicodeChar, fromUnicodeChar), UnicodeText)
 import Data.Data(Data)
 import Data.Functor.Classes(Eq1(liftEq), Ord1(liftCompare))
@@ -36,12 +41,60 @@ import GHC.Generics(Generic, Generic1)
 
 import Test.QuickCheck.Arbitrary(Arbitrary(arbitrary), Arbitrary1(liftArbitrary), arbitrary1)
 
+pattern FullBlock :: Char
+pattern FullBlock = '\x2588'
+
+pattern EmptyBlock :: Char
+pattern EmptyBlock = ' '
+
+pattern LeftHalfBlock :: Char
+pattern LeftHalfBlock = '\x258c'
+
+pattern RightHalfBlock :: Char
+pattern RightHalfBlock = '\x2590'
+
+
 -- | A data type that determines the state of the /row/ in a block.
 -- it determines the left and the right part of the row of the block.
 data Row a = Row {
     left :: a  -- ^ The left part of a row of the block.
   , right :: a  -- ^ The right part of the row of the block.
   } deriving (Bounded, Data, Eq, Foldable, Functor, Generic, Generic1, Ord, Read, Show, Traversable)
+
+pattern FullRow :: Row Bool
+pattern FullRow = Row True True
+
+pattern EmptyRow :: Row Bool
+pattern EmptyRow = Row False False
+
+pattern LeftRow :: Row Bool
+pattern LeftRow = Row True False
+
+pattern RightRow :: Row Bool
+pattern RightRow = Row False True
+
+-- | Convert the given 'Row' of 'Bool'eans to an 'Int' where the left 'Bool' has value 1, and the right one has value two. The four different 'Row's thus are mapped to integers from zero to three (both inclusive).
+rowValue
+  :: Row Bool  -- ^ The given 'Row' of 'Bool's to convert.
+  -> Int  -- ^ The corresponding numerical value.
+rowValue ~(Row l r) = b0 1 l .|. b0 2 r
+  where b0 = bool 0
+
+-- | Convert the given number to a 'Row' of 'Bool's. If the value
+-- is out of bounds, it is unspecified what will happen.
+toRow'
+  :: Int  -- ^ The given number to convert.
+  -> Row Bool  -- ^ The corresponding 'Row' of 'Bool's.
+toRow' i = Row (odd i) (i >= 0x02)
+
+-- | Convert the given number to a 'Row' of 'Bool's wrapped in a 'Just'.
+-- if the value is out of bounds, 'Nothing' is returned.
+toRow
+  :: Int -- ^ The given number to convert.
+  -> Maybe (Row Bool)  -- ^ The corresponding 'Row' of 'Bool's.
+toRow i
+  | i >= 0x00 && i <= 0x03 = Just (toRow' i)
+  | otherwise = Nothing
 
 instance Eq1 Row where
   liftEq cmp ~(Row xa xb) ~(Row ya yb) = cmp xa ya && cmp xb yb
@@ -93,7 +146,8 @@ instance Applicative Row where
     Row fl fr <*> Row l r = Row (fl l) (fr r)
 
 instance Applicative Block where
-    pure x = Block (pure x) (pure x)
+    pure x = Block px px
+      where px = pure x
     Block fu fl <*> Block u l = Block (fu <*> u) (fl <*> l)
 
 instance Arbitrary a => Arbitrary (Row a) where
@@ -114,12 +168,12 @@ instance Arbitrary1 Block where
 fromBlock
   :: Char  -- ^ The given 'Char'acter to convert to a 'Block' of 'Bool's.
   -> Maybe (Block Bool)  -- The equivalent 'Block' of 'Bool's wrapped in a 'Just' if such block exists; 'Nothing' otherwise.
-fromBlock ' ' = Just (Block (Row False False) (Row False False))
+fromBlock EmptyBlock = Just (Block (Row False False) (Row False False))
 fromBlock '\x2580' = Just (Block (Row True  True ) (Row False False))
 fromBlock '\x2584' = Just (Block (Row False False) (Row True  True ))
-fromBlock '\x2588' = Just (Block (Row True  True ) (Row True  True ))
-fromBlock '\x258c' = Just (Block (Row True  False) (Row True  False))
-fromBlock '\x2590' = Just (Block (Row False True ) (Row False True ))
+fromBlock FullBlock = Just (Block (Row True  True ) (Row True  True ))
+fromBlock LeftHalfBlock = Just (Block (Row True  False) (Row True  False))
+fromBlock RightHalfBlock = Just (Block (Row False True ) (Row False True ))
 fromBlock '\x2596' = Just (Block (Row False False) (Row True  False))
 fromBlock '\x2597' = Just (Block (Row False False) (Row False True ))
 fromBlock '\x2598' = Just (Block (Row True  False) (Row False False))
@@ -143,12 +197,12 @@ fromBlock' = fromJust . fromBlock
 filled
     :: Block Bool  -- ^ The given 'Block' of 'Bool's to convert to a 'Char'acter.
     -> Char  -- ^ The equivalent Unicode 'Char'acter for the given 'Block' of 'Bool's.
-filled (Block (Row False False) (Row False False)) = ' '
+filled (Block (Row False False) (Row False False)) = EmptyBlock
 filled (Block (Row True  True ) (Row False False)) = '\x2580'
 filled (Block (Row False False) (Row True  True )) = '\x2584'
-filled (Block (Row True  True ) (Row True  True )) = '\x2588'
-filled (Block (Row True  False) (Row True  False)) = '\x258c'
-filled (Block (Row False True ) (Row False True )) = '\x2590'
+filled (Block (Row True  True ) (Row True  True )) = FullBlock
+filled (Block (Row True  False) (Row True  False)) = LeftHalfBlock
+filled (Block (Row False True ) (Row False True )) = RightHalfBlock
 filled (Block (Row False False) (Row True  False)) = '\x2596'
 filled (Block (Row False False) (Row False True )) = '\x2597'
 filled (Block (Row True  False) (Row False False)) = '\x2598'
